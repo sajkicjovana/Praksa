0b864d8dffb82b00a88ad95d83d5024415b3bc9b
Jovana Sajkic <sajkicj2003@gmail.com>
2026-03-23 13:56:07 +0100

    Guard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,6 +1,17 @@
 import { CanActivateFn } from '@angular/router';
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from '../services/auth.services';
+import { inject } from '@angular/core';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const api: AuthService = inject(AuthService);
   
+  const router = inject(Router);
+
+  if(localStorage.getItem("token") == null)
+    // return false;
+      return router.navigate(["login"]);
+
   return true;
 };
--- src/app/guards/no-auth-guard.ts
+++ src/app/guards/no-auth-guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.services';
+import { inject } from '@angular/core';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
+  // const api: AuthService = inject(AuthService)
+    const router = inject(Router);
+  if(localStorage.getItem("token") != null)
+    return router.navigate(["/connections"]);
+    // return false;
+
   return true;
 };
